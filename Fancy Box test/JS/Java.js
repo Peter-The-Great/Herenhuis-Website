b7cf330ee5dbfc0e35bcc0125dc8c0b9da9ff4b9
Peter-The-Great <56716150+Peter-The-Great@users.noreply.github.com>
2019-12-02 21:51:03 +0100

    Nieuwe folder (Alleen voor Pjotr)
    Het is een folder voor de Fancybox Javascript/JQuery Plugin.

--- Fancy Box test/JS/Java.js
+++ Fancy Box test/JS/Java.js
@@ -1,4 +1,4 @@
-//Hier komen de basis dingen voor Fancybox
+//Here will be some basic things the fancybox plugin must do to specific images.
 $('[data-fancybox]').fancybox({
 		loop: true,//Here we will state that there will be a loop for the gallary
 		//here we state all the buttons that will be active in the fancybox data
