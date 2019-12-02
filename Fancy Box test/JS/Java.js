fff0c27692c9e02cc2e57dccb9c2d1ed844cb7d9
Peter-The-Great <56716150+Peter-The-Great@users.noreply.github.com>
2019-12-02 21:03:53 +0100

    Update Java.js

--- Fancy Box test/JS/Java.js
+++ Fancy Box test/JS/Java.js
@@ -1,1 +1,16 @@
 //Hier komen de basis dingen voor Fancybox
+$('[data-fancybox]').fancybox({
+		loop: true,//Here we will state that there will be a loop for the gallary
+		//here we state all the buttons that will be active in the fancybox data
+		buttons: [
+    "zoom",
+    "share",
+    //"slideShow",
+    //"fullScreen",
+    //"download",
+    "thumbs",
+    "close"
+  ],
+  //Here i state the transition effect.
+  transitionEffect: "circular",
+	});
