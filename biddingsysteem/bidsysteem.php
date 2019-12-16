26637152a6bec779aed596863aee9bbac3aa8007
lazororz <58169881+lazororz@users.noreply.github.com>
2019-12-16 11:33:34 +0100

    Add files via upload

--- biddingsysteem/bidsysteem.php
+++ biddingsysteem/bidsysteem.php
@@ -1,47 +1,51 @@
+
 <!DOCTYPE html>
 <html>
+<head>
+<title>bidsysteem</title>
+	
+</head>
 <body>
 
-
+<h1>plaats hier uw bod</h1>
+<form action="insert.php" method="POST">
+bid: <input type="number" name="bid" value="<?php echo $bid;?>" min="<?php $row["BID"];?>" placeholder="Minst meer dan € 100.000" required><br><br>
 Voornaam: <input type="text" name="voornaam" value="<?php echo $voornaam;?>"required><br><br>
 Achternaam: <input type="text" name="achternaam" value="<?php echo $achternaam;?>"required><br><br>
-E-mail: <input type="email" name="email" value="<?php echo $email;?>"required><br><br>
-bid: <input type="text" name="voornaam" value="<?php echo $bid;?>"required><br><br>
-<script>
-Asking Price: $1
-Bidder 1: Max Bid of $4,000 -> High Bid: $1,000
-Bidder 2: Max Bid of $3,000 -> High Bid: $3,000 -> [AUTO BIDDER 1] High Bid: $4,000
-Bidder 3: Max Bid of $8,000 -> High Bid: $5,000
-Bidder 4: Max Bid of $10,000 -> [AUTO BIDDER 3] High Bid: $8,000 -> High Bid: $9,000
+E-mail: <input type="email" name="email" value="<?php echo $email;?>" required><br><br>
+<input type="submit" name="submit">
+<input type="reset" name="reset">
+</form>
 
-</script>
-<?php  
-	$voornaam = $achternaam = $email = $bid =  "";
+<?php
+$servername = "localhost";
+$database = "80781_beroeps";
+$username = "80781";
+$password = "v6odf2zfX";
 
+$conn = mysqli_connect($servername, $username, $password, $database);
+	
+	
+	$sql= "SELECT `Voornaam`, `date`, `BID` FROM `BID` ORDER BY BID DESC";
+    $result = mysqli_query($conn, $sql);
+	if (mysqli_num_rows($result) >= 5) {
+    
 		
-		if ($_POST["submit"]) {
-	print "<h2>uw bid wordt geplaats</h2>";
-	$email = $_POST["email"];
-	$voornaam = $_POST["voornaam"];
-	$achternaam = $_POST["achternaam"];	
-
-	/*$mailtekst = "
-Geachte $voornaam $achternaam.
-
-Hierbij uw reserveringsgegevens:
-De gekozen datum is:.$datum
-De gekozen tijd is: $tijd
-Comment: $comment
-
-
-tot gauw, 
-
-kip & ei restaurant.";
-**/
-	mail("rik@remijn.org,$email", "Kip & Ei resevering ", $mailtekst, "From: no-reply@kipei.org");
-
-function placeBid($currentBidder,$highBid,$thisBidder,$thisBid,$increment)
-?>  
-
+	
+		echo "<table>";
+    while($row = mysqli_fetch_assoc($result)) {
+		echo "<tr>";
+        echo "<td>".$row["Voornaam"]."</td>" . "<td>" . $row["BID"]. "</td>" . "<td>" . $row["date"] . "</td>" . "<br>";
+		echo "</tr>";
+    }
+			//Close table
+		echo "</table>";
+} else {
+    echo "0 results";
+}
+sort ($row["BID"]);
+	
+	
+	?>
 </body>
 </html>
