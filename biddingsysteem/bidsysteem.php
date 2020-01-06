80bfaabaac4dfb944584650359c5ca1f9c648769
lazororz <58169881+lazororz@users.noreply.github.com>
2020-01-06 11:38:43 +0100

    Add files via upload

--- biddingsysteem/bidsysteem.php
+++ biddingsysteem/bidsysteem.php
@@ -1,4 +1,16 @@
+<?php
+$servername = "localhost";
+$database = "80781_beroeps";
+$username = "80781";
+$password = "Qu1p3*v6";
 
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if ($conn -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,9 +19,24 @@
 </head>
 <body>
 
+	<?php
+	$sql= "SELECT `bod` FROM `BID` ORDER BY `bod` DESC LIMIT 1";
+	$result = mysqli_query($conn, $sql);
+	$antwoord = mysqli_fetch_array($result);
+	
+	/*if (!$conn -> query($sql)) {
+	  echo("Error description: " . $conn -> error);
+	}
+	else {echo $sql;}*/
+	
+	$minbid = ($antwoord['bod'] + 10000);
+	//echo "<p>$minbid</p>";
+
+	
+	?>
 <h1>plaats hier uw bod</h1>
-<form action="insert.php" method="POST">
-bid: <input type="number" name="bid" value="<?php echo $bid;?>" min="<?php $row["BID"];?>" placeholder="Minst meer dan € 100.000" required><br><br>
+<form action="insert.php" action="mail.php"  method="POST">
+bid: <input type="number" name="bid" value="<?php echo $bid;?>" min="<?php echo $minbid;?>"placeholder="Minst meer dan € 100.000" required><br><br>
 Voornaam: <input type="text" name="voornaam" value="<?php echo $voornaam;?>"required><br><br>
 Achternaam: <input type="text" name="achternaam" value="<?php echo $achternaam;?>"required><br><br>
 E-mail: <input type="email" name="email" value="<?php echo $email;?>" required><br><br>
@@ -18,24 +45,18 @@ E-mail: <input type="email" name="email" value="<?php echo $email;?>" required><
 </form>
 
 <?php
-$servername = "localhost";
-$database = "80781_beroeps";
-$username = "80781";
-$password = "v6odf2zfX";
 
-$conn = mysqli_connect($servername, $username, $password, $database);
 	
-	
-	$sql= "SELECT `Voornaam`, `date`, `BID` FROM `BID` ORDER BY BID DESC";
+	$sql= "SELECT `Voornaam`, `date`, `bod` FROM `BID` ORDER BY bod DESC";
     $result = mysqli_query($conn, $sql);
-	if (mysqli_num_rows($result) >= 5) {
+	if (mysqli_num_rows($result)  > 0 ) {
     
 		
 	
 		echo "<table>";
     while($row = mysqli_fetch_assoc($result)) {
 		echo "<tr>";
-        echo "<td>".$row["Voornaam"]."</td>" . "<td>" . $row["BID"]. "</td>" . "<td>" . $row["date"] . "</td>" . "<br>";
+        echo "<td>".$row["Voornaam"]."</td>" . "<td>" . $row["bod"]. "</td>" . "<td>" . $row["date"] . "</td>" . "<br>";
 		echo "</tr>";
     }
 			//Close table
@@ -43,7 +64,7 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 } else {
     echo "0 results";
 }
-sort ($row["BID"]);
+sort($row["bod"]);
 	
 	
 	?>
--- biddingsysteem/insert.php
+++ biddingsysteem/insert.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $database = "80781_beroeps";
 $username = "80781";
-$password = "v6odf2zfX";
+$password = "Qu1p3*v6";
 
 $conn = mysqli_connect($servername, $username, $password, $database);
 
@@ -13,7 +13,7 @@ $email = $_POST["email"];
 $date = date("Y-m-d");
 	
 
-		$sql = "INSERT INTO BID (Voornaam, Achternaam, BID, E_mail, date)
+		$sql = "INSERT INTO BID (Voornaam, Achternaam, bod, E_mail, date)
 VALUES ('$voornaam', '$achternaam', '$bid', '$email', '$date');";
 
 	mysqli_query($conn, $sql);
